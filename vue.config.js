b29855900040a86fa9181519cc26ec7849766d7c
UFang <lokony82@g4e.npust.edu.tw>
2022-07-21 15:49:11 +0800

    修改vue.config.js，新增 deploy.sh 檔案

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,15 @@
-// const { defineConfig } = require('@vue/cli-service')
-// module.exports = defineConfig({
-//   transpileDependencies: true
-// })
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true
+})
+
 module.exports = {
-  // publicPath 為 project-name，根目錄地址為上傳的網域
-  publicPath: '/pickit/'
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/pickit/' // pickit 為 repo 名稱
+    : '/'
 }
+
+// module.exports = {
+//   // publicPath 為 project-name，根目錄地址為上傳的網域
+//   publicPath: '/pickit/'
+// }
