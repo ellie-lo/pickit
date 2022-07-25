5a0755f24ae07227fc2323881392c0582b5d0bc1
UFang <lokony82@g4e.npust.edu.tw>
2022-07-25 14:15:05 +0800

    修改home按鍵跑版修正

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,9 +4,9 @@
       <p class="home-title mt-5">你是否也有選擇性障礙?<br>早餐、午餐、晚餐、下午茶<br>
 猶豫不決中選擇想吃的店家或食物<br>
 選吧! 幫你隨機決定</p>
-      <ul class="checkbox mt-5">
-        <li><router-link to="/store" class="checkbox-list me-5">店家選項</router-link></li>
-        <li><router-link to="/food" class="checkbox-list checkbox-list2">食物類型</router-link></li>
+      <ul class="checkbox mt-5 row">
+        <li class="col-12 col-sm-6 mb-5"><router-link to="/store" class="checkboxlist checkbox-list">店家選項</router-link></li>
+        <li class="col-12 col-sm-6 mb-5"><router-link to="/food" class="checkboxlist checkbox-list2">食物類型</router-link></li>
       </ul>
     </div>
   </div>
